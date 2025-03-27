b8389ea78cc40d69c8b66e427502d3e970c4cccb
AshongCyril <cashong005@st.ug.edu.gh>
2025-03-27 11:57:16 +0000

    Made some modification to the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
+import './css/App.css'
 import MovieCard from './component/MovieCard'
+import NavBar from './component/NavBar'
+import Favourites from './pages/Favourites'
+import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
+import { MovieProvider } from './contexts/MovieContext'
 
 
 function App() {
   return (
-    <>
-      <MovieCard movie={{ title: "Nii's Film", release_date: "2024" }} />
-    </>
+    <MovieProvider>
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favourites' element={<Favourites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+
   )
 }
 
